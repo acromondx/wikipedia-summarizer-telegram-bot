84dfddc36df062b7d2f7b9a2fe404c533809dec9
Gideon Acromond <50566307+acromondx@users.noreply.github.com>
2022-06-29 01:38:18 +0000

    added main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+# import modules
+import telegram.ext
+import os
+import wikipedia
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+# constants
+PORT = int(os.environ.get("PORT", 443))
+TOKEN = "********************************" #removed for obvious reasons
+
+def create_disambiguation_keyboard(disambiguation_options):
+   keyboard = []
+
+   for option in disambiguation_options:
+     new_option = [InlineKeyboardButton(option, callback_data='ERROR;DISAMBIGUATION;' + option)]
+     keyboard.append(new_option)
+
+   return InlineKeyboardMarkup(keyboard)
+
+# function to fetch wikipedia articles
+def wikipedia_page(query):
+    try:
+        result = wikipedia.summary(query, sentences=2, auto_suggest=False)
+    except wikipedia.exceptions.DisambiguationError as e:
+        # na = e.options
+        na=create_disambiguation_keyboard(e.options)
+
+        return f"""
+                    Please specify your query for {query}:
+                    \n Example: If you want {na[1]}, Copy and paste (or just type) {na[1]}
+                    \n\n{na}
+        
+        """
+
+    return f"""{query}\n\n{result}"""
+
+
+def start(update, context):
+    update.message.reply_text(
+        """
+Wikipedia-mini
+    """
+    )
+
+
+def handle_message(update, context):
+    update.message.reply_text(wikipedia_page(update.message.text))
+    update.message.reply_text(wikipedia.page(update.message.txt).images[0])
+
+
+updater = telegram.ext.Updater(TOKEN, use_context=True)
+disp = updater.dispatcher
+
+
+
+disp.add_handler(telegram.ext.CommandHandler("start", start))
+disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
+
+# for deployment
+# updater.start_webhook(
+#     listen="0.0.0.0",
+#     port=int(PORT),
+#     url_path=TOKEN,
+#     webhook_url="https://acronvisible.herokuapp.com/" + TOKEN,
+# )
+
+# for local deployment 
+updater.start_polling()
+
+updater.idle()
