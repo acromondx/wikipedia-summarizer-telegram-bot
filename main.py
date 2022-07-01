a3a9710c78b73b210aabc3b7f138b0b59d0a5316
Gideon Acromond <50566307+acromondx@users.noreply.github.com>
2022-07-01 16:36:02 +0000

    added more functionalities

--- main.py
+++ main.py
@@ -2,28 +2,33 @@
 import telegram.ext
 import os
 import wikipedia
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 
 # constants
 PORT = int(os.environ.get("PORT", 443))
-TOKEN = "********************************" #removed for obvious reasons
+TOKEN = "[YourTelegramToken]"
 
+# mutliple results display buttons
 def create_disambiguation_keyboard(disambiguation_options):
-   keyboard = []
+    keyboard = []
 
-   for option in disambiguation_options:
-     new_option = [InlineKeyboardButton(option, callback_data='ERROR;DISAMBIGUATION;' + option)]
-     keyboard.append(new_option)
+    for option in disambiguation_options:
+        new_option = [
+            InlineKeyboardButton(option, callback_data="ERROR;DISAMBIGUATION;" + option)
+        ]
+        keyboard.append(new_option)
 
-   return InlineKeyboardMarkup(keyboard)
+    return InlineKeyboardMarkup(keyboard)
+
 
 # function to fetch wikipedia articles
-def wikipedia_page(query):
+def wikipedia_page():
+    query = updater.message.text
     try:
         result = wikipedia.summary(query, sentences=2, auto_suggest=False)
     except wikipedia.exceptions.DisambiguationError as e:
         # na = e.options
-        na=create_disambiguation_keyboard(e.options)
+        na = create_disambiguation_keyboard(e.options)
 
         return f"""
                     Please specify your query for {query}:
@@ -32,25 +37,30 @@ def wikipedia_page(query):
         
         """
 
-    return f"""{query}\n\n{result}"""
+    return f"{query}\n\n{result}"
 
 
+# Bot start function
 def start(update, context):
     update.message.reply_text(
         """
-Wikipedia-mini
-    """
+
+Wikipedia Summarizer is a bot that summarises Wikipedia articles for you.
+
+<b>USAGE:</b>
+To search for articles, type and send your keyword.
+    """,
+        parse_mode=ParseMode.HTML,
     )
 
 
 def handle_message(update, context):
     update.message.reply_text(wikipedia_page(update.message.text))
-    update.message.reply_text(wikipedia.page(update.message.txt).images[0])
+    update.message.reply_text(wikipedia.page(update.message.text).images[0])
 
 
 updater = telegram.ext.Updater(TOKEN, use_context=True)
 disp = updater.dispatcher
-
 
 
 disp.add_handler(telegram.ext.CommandHandler("start", start))
@@ -61,10 +71,9 @@ disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_m
 #     listen="0.0.0.0",
 #     port=int(PORT),
 #     url_path=TOKEN,
-#     webhook_url="https://acronvisible.herokuapp.com/" + TOKEN,
+#     webhook_url="[YourHerokuSite]/" + TOKEN,
 # )
 
-# for local deployment 
+# for local deployment
 updater.start_polling()
-
 updater.idle()
